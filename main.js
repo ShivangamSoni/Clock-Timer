e5c5f44db3084c7394bff02aa208a9605fc3b324
Shivangam Soni <shivangamsoni99@gmail.com>
2021-09-17 17:05:28 +0530

    UI 100% Complete JS Part Remaining

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
+// Clock
 const hours = document.querySelector(".clock-hours");
 const minutes = document.querySelector(".clock-minutes");
 const seconds = document.querySelector(".clock-seconds");
 const ampm = document.querySelector(".clock-ampm");
 
+function leadingZero(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
 function getTime() {
   const date = new Date();
-  const h = date.getHours();
+  let h = date.getHours();
   const m = date.getMinutes();
   const s = date.getSeconds();
   const am = h >= 12 ? "pm" : "am";
 
-  hours.textContent = h % 12 < 10 ? `0${h % 12}` : h % 12;
-  minutes.textContent = m < 10 ? `0${m}` : m;
-  seconds.textContent = s < 10 ? `0${s}` : s;
+  h = h % 12 || 12;
+
+  hours.textContent = leadingZero(h);
+  minutes.textContent = leadingZero(m);
+  seconds.textContent = leadingZero(s);
   ampm.textContent = am;
 }
 
 setInterval(getTime, 1000);
 
-/*
-hsl(268, 88%, 26%)
-hsl(290, 100%, 50%)
-*/
+// Custom Select
+const selectedList = document.querySelectorAll(".selected");
+const optionsList = document.querySelectorAll(".option");
+
+function toggleSelect(container) {
+  container.classList.toggle("active");
+}
+
+function setSelect(e) {
+  const option = e.target;
+  toggleSelect(option.parentElement);
+
+  const label = option.querySelector("label");
+  const selected = option.parentElement.nextElementSibling;
+  selected.textContent = label.textContent;
+
+  const radio = option.querySelector("input");
+  console.log(radio.getAttribute("name") + ": " + label.textContent);
+}
+
+selectedList.forEach((select) => select.addEventListener("click", () => {
+  toggleSelect(select.previousElementSibling);
+}));
+
+optionsList.forEach((option) => option.addEventListener("click", setSelect));
