3e10af288a5d4df5e3588b777b4add63b12dd64a
Shivangam Soni <shivangamsoni99@gmail.com>
2021-09-22 22:31:35 +0530

    Added Alarm Functionality(Partially)

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const seconds = document.querySelector(".clock-seconds");
 const ampm = document.querySelector(".clock-ampm");
 const colons = document.querySelectorAll(".clock-colon");
 
+(function initialize() {
+  colons[0].style.animationPlayState = "running";
+  colons[1].style.animationPlayState = "running";
+  setInterval(getTime, 1000);
+  getTime();
+})();
+
 function getTime() {
   function leadingZero(time) {
     return time < 10 ? `0${time}` : time;
@@ -24,12 +31,84 @@ function getTime() {
   ampm.textContent = am;
 }
 
-(function initialize() {
-  colons[0].style.animationPlayState = "running";
-  colons[1].style.animationPlayState = "running";
-  setInterval(getTime, 1000);
-  getTime();
-})();
+// Alarm
+const notification = document.querySelector(".timer-notification");
+const LOCAL_STORAGE_KEY_ALARM_TIME = "app.alarm";
+const alarmTimes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_ALARM_TIME)) || { sleep: 22 };
+setSelected();
+setAlarm();
+
+function setTimeValues(alarmFor, time) {
+  const hours = time.split(" ").reduce((h, a) => {
+    h = Number(h);
+    if (a === "AM") {
+      return h === 12 ? 0 : h;
+    } else {
+      return h === 12 ? 12 : h + 12;
+    }
+  });
+
+  if (alarmFor.trim() === "wake") {
+    alarmTimes.wake = hours;
+  } else if (alarmFor.trim() === "lunch") {
+    alarmTimes.lunch = hours;
+  } else {
+    alarmTimes.sleep = hours;
+  }
+
+  localStorage.setItem(LOCAL_STORAGE_KEY_ALARM_TIME, JSON.stringify(alarmTimes));
+  setAlarm();
+}
+
+function setAlarm() {
+  if (alarmTimes === {}) {
+    return;
+  }
+
+  for (let key in alarmTimes) {
+    const current = new Date();
+    const timeToAlarm = new Date();
+    timeToAlarm.setHours(alarmTimes[key]);
+    timeToAlarm.setMinutes(0);
+    timeToAlarm.setSeconds(0);
+
+    const difference = timeToAlarm.getTime() - current.getTime();
+
+    if (difference > 0) {
+      setTimeout(alarm.bind(null, key), difference);
+    }
+  }
+}
+
+function alarm(key) {
+  const currentSection = document.querySelector(".sub-section.active");
+  const newSection = document.querySelector(`.sub-section[data-section-for="${key}"]`);
+  const notificationToShow = notification.querySelector(`span[data-for="${key}"]`);
+  currentSection.classList.remove("active");
+  newSection.classList.add("active");
+
+  notification.classList.toggle("hide");
+  notificationToShow.classList.toggle("hide");
+  setTimeout(() => {
+    notification.classList.toggle("hide");
+    setTimeout(() => {
+      notificationToShow.classList.toggle("hide");
+    }, 1000);
+  }, 3000);
+}
+
+function setSelected() {
+  if (alarmTimes === {}) return;
+
+  for (let key in alarmTimes) {
+    if (alarmTimes[key] !== null) {
+      const selected = document.querySelector(`div[data-select-for="${key}"]`);
+      const time = alarmTimes[key] % 12 || 0;
+      const am = alarmTimes[key] >= 12 ? "PM" : "AM";
+      selected.textContent = `${time} ${am}`;
+    }
+  }
+}
 
 // Custom Select
 const selectedList = document.querySelectorAll(".selected");
@@ -48,7 +127,8 @@ function setSelect(e) {
   selected.textContent = label.textContent;
 
   const radio = option.querySelector("input");
-  console.log(radio.getAttribute("name") + ": " + label.textContent);
+
+  setTimeValues(radio.getAttribute("name"), label.textContent);
 }
 
 selectedList.forEach((select) =>
